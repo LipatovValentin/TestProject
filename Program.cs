362281ea536c1ec505a473b3bdf3b0138b61bb5e
LipatovValentin <56578070+LipatovValentin@users.noreply.github.com>
2021-03-13 12:47:30 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,146 @@ namespace TestProject
         void Enqueue(T item);
         int Count { get; }
     }
+    public class Node<T>
+    {
+        public Node(T value)
+        {
+            Value = value;
+        }
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+    }
+    public class QueueWithNode<T> : IQueue<T>
+    {
+        private Node<T> _head;
+        private Node<T> _tail;
+        private int _count;
+        private int _version;
+        public Node<T> Head { get => _head; }
+        public Node<T> Tail { get => _tail; }
+        public int Count { get => _count; }
+        public int Version { get => _version; }
+        public QueueWithNode()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+            _version = 0;
+        }
+        public void Enqueue(T item)
+        {
+            Node<T> tempNode = _tail;
+            _tail = new Node<T>(item);
+            if (_count == 0 || _head == null)
+            {
+                _head = _tail;
+            }
+            else
+            {
+                tempNode.Next = _tail;
+            }
+            _count = _count + 1;
+            _version = _version + 1;
+        }
+        public T Dequeue()
+        {
+            if (_count == 0 || _head == null)
+            {
+                throw new InvalidOperationException();
+            }
+            T obj = _head.Value;
+            _head = _head.Next;
+            _count = _count - 1;
+            _version = _version + 1;
+            return obj;
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new EnumeratorWithNode<T>(this);
+        }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return new EnumeratorWithNode<T>(this);
+        }
+    }
+    public struct EnumeratorWithNode<T> : IEnumerator<T>, IDisposable, IEnumerator
+    {
+        private QueueWithNode<T> _queue;
+        private Node<T> _current;
+        private int _version;
+        internal EnumeratorWithNode(QueueWithNode<T> queue)
+        {
+            _queue = queue;
+            _version = _queue.Version;
+            _current = null;
+        }
+        public void Dispose()
+        {
+            _current = null;
+        }
+        public bool MoveNext()
+        {
+            if (_version != _queue.Version)
+            {
+                throw new InvalidOperationException();
+            }
+            if (_queue == null)
+            {
+                return false;
+            }
+            if (_queue.Head == null)
+            {
+                return false;
+            }
+            if (_current == null)
+            {
+                _current = _queue.Head;
+                return true;
+            }
+            else
+            {
+                if (_current.Next != null)
+                {
+                    _current = _current.Next;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public T Current
+        {
+            get
+            {
+                if (_current == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                return _current.Value;
+            }
+        }
+        object IEnumerator.Current
+        {
+            get
+            {
+                if (_current == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                return (object)_current.Value;
+            }
+        }
+        void IEnumerator.Reset()
+        {
+            if (_version != _queue.Version)
+            {
+                throw new InvalidOperationException();
+            }
+            _current = null;
+        }
+    }
     public class Queue<T> : IQueue<T>
     {
         private T[] _array;
