2f5c6d6cd9377d5947cc77c8cc9b58c4ed0a03bb
LipatovValentin <56578070+LipatovValentin@users.noreply.github.com>
2021-03-13 17:23:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,12 +12,12 @@ namespace TestProject
     }
     public class Node<T>
     {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
         public Node(T value)
         {
             Value = value;
         }
-        public T Value { get; set; }
-        public Node<T> Next { get; set; }
     }
     public class QueueWithNode<T> : IQueue<T>
     {
